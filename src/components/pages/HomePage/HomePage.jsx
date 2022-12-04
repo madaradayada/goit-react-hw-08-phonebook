ede53cd8e61536d7e22077ed7b7aba8bf1a63c07
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-12-04 11:07:02 +0200

    finish_for_check

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import { Title } from './HomePageStyled';
 
 export const HomePage = () => {
-  return <Title>Welcome to the best phone contact storage app. Service offers the creation of a private user where he can add, delete contacts. For new users, we offer to go through a small Registration in the registration tab.</Title>;
+  return <Title>Welcome to the my phone contact storage app.😀Register to get started.😉Good luck guys.😎</Title>;
 };
